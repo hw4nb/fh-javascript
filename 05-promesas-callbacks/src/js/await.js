4bed9ad6e65795cd135f6a80e0ab08cc09e28b30
Yobel Tayson <rotkyobel@gmail.com>
2021-06-26 15:06:07 -0500

    for await - if await

--- 05-promesas-callbacks/src/js/await.js
+++ 05-promesas-callbacks/src/js/await.js
@@ -1,6 +1,8 @@
 import { buscarHeroeAsync, buscarHeroe } from "./promesas";
 
 const heroesIds = ['capi', 'iron', 'spider'];
+// const heroesPromesas = heroesIds.map(id => buscarHeroe(id));
+const heroesPromesas = heroesIds.map(buscarHeroe);
 
 export const obtenerHeroesArr = async() => await Promise.all(heroesIds.map(buscarHeroe));
 
@@ -16,4 +18,28 @@ export const obtenerHeroeAwait = async (id) => {
       poder: 'Sin poder'
     }
   }
+}
+
+export const heroesCiclo = async () => {
+  console.time('HeroesCiclo');
+
+  // heroesPromesas.forEach(async p => console.log(await p))
+
+  for await(const heroe of heroesPromesas) {
+    console.log(heroe);
+  }
+
+  console.timeEnd('HeroesCiclo');
+  // const heroes = await Promise.all(heroesPromesas);
+  // heroes.forEach(heroe => console.log(heroe))
+}
+
+export const heroeIfAwait = async(id) => {
+
+  if((await buscarHeroeAsync(id)).nombre === 'Ironman') {
+    console.log('Es el mejor de todos');
+  } else {
+    console.log('Naaaaa');
+  }
+
 }
