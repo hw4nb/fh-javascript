ea82362d67466503d6a86f566f1da34da112bfd1
Yobel Tayson <rotkyobel@gmail.com>
2021-06-26 14:37:47 -0500

    manego de errores

--- 05-promesas-callbacks/src/js/await.js
+++ 05-promesas-callbacks/src/js/await.js
@@ -3,3 +3,17 @@ import { buscarHeroeAsync, buscarHeroe } from "./promesas";
 const heroesIds = ['capi', 'iron', 'spider'];
 
 export const obtenerHeroesArr = async() => await Promise.all(heroesIds.map(buscarHeroe));
+
+export const obtenerHeroeAwait = async (id) => {
+
+  try {
+    const heroe = await buscarHeroeAsync(id);
+    return heroe;
+  } catch (err) {
+    console.log('Catch!!');
+    return {
+      nombre: 'Sin nombre',
+      poder: 'Sin poder'
+    }
+  }
+}
