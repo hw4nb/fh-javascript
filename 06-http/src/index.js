71c056b7a9e04ac6f1312b501a51bc43d35d9c82
Yobel Tayson <rotkyobel@gmail.com>
2021-06-28 11:07:01 -0500

    crear usuario

--- 06-http/src/index.js
+++ 06-http/src/index.js
@@ -1,17 +1,5 @@
+// import { init } from "./js/chistes-page";
+import { init } from './js/usuarios-page'
 
-const jokeUrl = 'https://api.chucknorris.io/jokes/random';
 
-// fetch(jokeUrl).then(resp => {
-
-//   resp.json().then(({id, value})=> {
-//     console.log(id);
-//     console.log(value);
-//   });
-
-// })
-
-fetch(jokeUrl)
-  .then(resp => resp.json())
-  .then(({id, value}) => {
-    console.log(id, value)
-  })
+init();
